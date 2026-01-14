6b735651191ea24bf1f13d3ab0bc14408a50aabb
Jørgen Andresen Osberg <jorgen.andresen.osberg@entur.org>
2026-01-14 11:37:06 +0100

    Add tarball SHAs to formula

--- Formula/oav.rb
+++ Formula/oav.rb
@@ -9,19 +9,19 @@ class Oav < Formula
   on_macos do
     on_intel do
       url "https://github.com/entur/openapi-validator-cli/releases/download/v#{version}/oav-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_ME"
+      sha256 "sha256:afeecb924ba8e122aa2635b02232cf5dbd08a4530e3fd44e7eafc083a2780599"
     end
 
     on_arm do
       url "https://github.com/entur/openapi-validator-cli/releases/download/v#{version}/oav-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_ME"
+      sha256 "sha256:f1d5b519d9211ff77ace6c34621cdfe324b9858553627309e413114ebed1f8cb"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/entur/openapi-validator-cli/releases/download/v#{version}/oav-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_ME"
+      sha256 "sha256:5dd06069b2ce3e7e0cc2e8cdddb8c95b3fe4b38e13886387dd2bac3e4fc3be76"
     end
   end
 
